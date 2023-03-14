23e1da885e757d434a2ecd91fc973acd36a0c492
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-03-14 23:09:08 +0200

    added categories

--- app/src/main/java/com/RivskyInc/wallpy/fragments/CategoryFragment.kt
+++ app/src/main/java/com/RivskyInc/wallpy/fragments/CategoryFragment.kt
@@ -1,6 +1,6 @@
 package com.RivskyInc.wallpy.fragments
 
-import android.R
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
@@ -8,33 +8,39 @@ import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView.BufferType
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-
+import com.RivskyInc.wallpy.WallpaperDetailActivity
 import com.RivskyInc.wallpy.databinding.FragmentCategoryBinding
 
 
 class CategoryFragment : Fragment() {
 
-    private lateinit var binding : FragmentCategoryBinding
+    private lateinit var binding: FragmentCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCategoryBinding.inflate(layoutInflater, container, false )
+        binding = FragmentCategoryBinding.inflate(layoutInflater, container, false)
 
-//       val  buttonMinimalism =   binding.buttonMinimalism
-//        val text = SpannableString("M I N I M A L I S M ")
-//        // make "Clicks" (characters 0 to 5) Red
-//        // make "Clicks" (characters 0 to 5) Red
-//        text.setSpan(ForegroundColorSpan(Color.BLACK), 0, 10, 0)
-//        // make "Here" (characters 6 to 10) Blue
-//        // make "Here" (characters 6 to 10) Blue
-//        text.setSpan(ForegroundColorSpan(Color.WHITE), 11, 16, 0)
-//        // shove our styled text into the Button
-//        // shove our styled text into the Button
-//        buttonMinimalism.setText(text, BufferType.SPANNABLE)
+        binding.textButtonNature.setOnClickListener {
+            val intent = Intent(this@CategoryFragment.context, WallpaperDetailActivity::class.java)
+
+            startActivity(intent)
+
+        }
+       val  buttonMinimalism =   binding.buttonMinimalism
+        val text = SpannableString("M I N I M A L I S M ")
+        // make "Clicks" (characters 0 to 5) Red
+        // make "Clicks" (characters 0 to 5) Red
+        text.setSpan(ForegroundColorSpan(Color.BLACK), 0, 10, 0)
+        // make "Here" (characters 6 to 10) Blue
+        // make "Here" (characters 6 to 10) Blue
+        text.setSpan(ForegroundColorSpan(Color.WHITE), 11, 16, 0)
+        // shove our styled text into the Button
+        // shove our styled text into the Button
+        buttonMinimalism.setText(text, TextView.BufferType.SPANNABLE)
 
         return binding.root
     }
