8594c06d1d9d6299be45057a13eaacf4e6d72527
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-03-16 16:25:42 +0200

    Interstitial test ad added

--- app/src/main/java/com/RivskyInc/wallpy/MainActivity.kt
+++ app/src/main/java/com/RivskyInc/wallpy/MainActivity.kt
@@ -1,23 +1,50 @@
 package com.RivskyInc.wallpy
 
+import android.content.ContentValues.TAG
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.RivskyInc.wallpy.databinding.ActivityMainBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
 
+    private lateinit var binding : ActivityMainBinding
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         MobileAds.initialize(this)
+
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError.toString())
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
     }
 
     @Deprecated("Deprecated in Java")
